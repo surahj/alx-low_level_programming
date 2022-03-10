aa61cfd7668e97a321c1702ec4839c7a4cb27019
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-10 22:19:38 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "Anything", "with proper grammar, but the outcome is a piece of art, ");
+	fprintf(stderr, "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
