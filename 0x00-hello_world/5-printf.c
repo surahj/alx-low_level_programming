5adf5825399e498c4e6e15c0ae7e18a5c5f689a9
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-10 21:23:59 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * main - return string
+ * Return: 1 if the printf function is true
+ */
+int main(void)
+{
+	printf ("%s", "with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
