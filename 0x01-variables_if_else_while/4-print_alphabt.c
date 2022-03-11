80e949161f109114dc49c2f3a65acf9a22a278fa
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-11 23:51:37 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,7 +9,11 @@ int main(void)
 	int letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+        {
+                if (letter == q || letter == e)
+                        continue;
 		putchar(letter);
+        }
 
 	putchar('\n');
 	return (0);
