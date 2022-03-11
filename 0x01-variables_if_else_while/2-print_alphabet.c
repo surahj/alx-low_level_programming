1d08d0c749021ea14f296a9cd20d599113051b69
surahj <odebodeopeyemi@gmail.com>
2022-03-11 14:01:32 -0800

    Edit file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Always 0
+ */
 int main(void)
 {
-	for(int x = 'A'; x <= 'Z'; x++)
-	{
-		int lower_x = tolower(x);
-		putchar(lower_x);
+		printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 	}
-}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
