f3c6fe2321aa3061b5023bfc9a8500ca6b61b2d8
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-12 00:57:06 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/**
+ * main - Prints base 10 number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
