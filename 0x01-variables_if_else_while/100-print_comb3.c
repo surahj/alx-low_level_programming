f2e0bdac7eba19e155ade8b4fd44708b72bec449
surahj <odebodeopeyemi@gmail.com>
2022-03-12 15:16:19 -0800

    Edit 100-print_comb3.c  file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,19 @@
  */
 int main(void)
 {
-	unsigned int i;
-	unsigned int j;
+	 int i;
+	 int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-			putchar(i);
-			putchar(j);
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 9)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
