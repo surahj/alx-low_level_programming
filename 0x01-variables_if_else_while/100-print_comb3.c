ea74571ddeb3db8342323ca30983a56f1a97839f
surahj <odebodeopeyemi@gmail.com>
2022-03-12 14:01:17 -0800

    Add 100-print_comb3.c file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: print combination of two number
+ *Return: 0 end of the function
+ */
+int main(void)
+{
+	unsigned char i;
+	unsigned char j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
