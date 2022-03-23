38694ca2bdb703e95f4fbba4760fcb80e6e0857f
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-23 20:06:33 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	size_t i, n;
+	size_t i;
 
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	
+	dest[i] = '\0';
 
 	return dest;
 }
