3b0f1d12095379f0cf8e794015ea66696902b1be
surahj <odebodeopeyemi@gmail.com>
2022-03-24 04:07:15 -0700

    Fix logic error in 3-main.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+/**
+ * _strcmp - compare two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: 0 if both string equal,non-zero otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j= 0;
+
+	while ((s1[i] != '\0' && s2[j] != '\0') && s1[j] == s2[j])
+	{
+		i++;
+		j++;
+	}
+
+	if (s1[i] == s2[j])
+		return (0);
+
+	else
+		return (s1[i] - s2[j]);
+}
