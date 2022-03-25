94940f0a7cd9afaaf0827abaafc27255a0ff31bc
surahj <odebodeopeyemi@gmail.com>
2022-03-24 23:47:57 -0700

    Add 101-print_number.c file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,8 +9,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *str);
 
-
+void print_number(int n);
 char *leet(char *str);
-
+int _putchar(char c);
 
 #endif
