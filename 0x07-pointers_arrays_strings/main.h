3b73fcd676840e6a894192ffb16af641d4c53460
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-03-29 15:51:32 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,5 @@ char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
+
+void print_diagsums(int *a, int size);
