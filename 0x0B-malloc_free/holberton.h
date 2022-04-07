87c68d8819c494f82c46151506ce311c1ed4d681
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-07 15:55:36 +0100

    Update holberton.h

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,9 +2,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
-<<<<<<< HEAD
 char *argstostr(int ac, char **av);
-=======
+char **strtow(char *str);
 
 void free_grid(int **grid, int height);
->>>>>>> 7d4976b2e5d6bab92a52c0f43b79805b36f5cd88
