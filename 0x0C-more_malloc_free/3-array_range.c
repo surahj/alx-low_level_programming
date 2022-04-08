3dd5b405793ec2780edf96f044cc3ca5edf6bedd
surahj <odebodeopeyemi@gmail.com>
2022-04-08 07:23:06 -0700

    Fix logical error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,16 +10,16 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int i;
+	int i, size = (max - min) + 1;
 
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min) + 1);
+	ptr = malloc(sizeof(int) * size);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= (max - min); i++)
+	for (i = 0; i <= size; i++)
 		ptr[i] = min++;
 	return (ptr);
 }
