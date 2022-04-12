cb751809f795f6dbd68a6512f4cf693b54dad158
surahj <odebodeopeyemi@gmail.com>
2022-04-12 10:50:51 -0700

    Add a function.that create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,10 +1,12 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the drug
+ * Return: a new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
