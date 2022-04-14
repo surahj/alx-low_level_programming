10a03ea35ef6d6f0cd3a5cf6d14e7b8d7d2302c5
surahj <odebodeopeyemi@gmail.com>
2022-04-14 09:44:30 -0700

    Fox error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ptr, int));
 
-		if (!(i == n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
