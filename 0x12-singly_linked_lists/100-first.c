ddca50c78bf0e1121a2bc4c605202c1cc19173d4
surahj <odebodeopeyemi@gmail.com>
2022-04-22 14:24:01 -0700

    Add function that execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * startUp -execute function before main
+ * Return: nothing
+ */
+
+void startUp(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
