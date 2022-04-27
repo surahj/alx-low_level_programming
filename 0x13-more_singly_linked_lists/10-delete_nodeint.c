00e5480e557333fbfc1df17838807d21759d0898
surahj <odebodeopeyemi@gmail.com>
2022-04-27 08:04:57 -0700

    Fix merging conflict

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 0; i < index - 1; i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (temp->next == NULL)
 			return (-1);
