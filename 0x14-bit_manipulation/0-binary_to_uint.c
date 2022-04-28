0d6b081196f92e983b3cfb7dd642385366be78d8
surahj <odebodeopeyemi@gmail.com>
2022-04-28 16:22:03 -0700

    Add 0-binary_to_uint.c file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+unsigned int sqr(unsigned int n, unsigned int power);
+
+/**
+ * sqr - find the sqare of a number
+ * @n: number to find its square
+ * @power: power of number to be square
+ * Return: the squared number
+ */
+
+unsigned int sqr(unsigned int n, unsigned int power)
+{
+	unsigned int s = 1, i;
+
+	if (power == 0)
+		return (1);
+
+	for (i = 0; i < power; i++)
+		s *= n;
+
+	return (s);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 0, len = 0, uint = 0;
+
+	while (b[i++])
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		uint += ((b[i] - '0') * sqr(2, (len - i - 1)));
+	}
+
+	return (uint);
+}
