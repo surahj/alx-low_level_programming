1a26a21b8728397738f41f3b775323364746ef63
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-29 14:50:59 +0100

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
