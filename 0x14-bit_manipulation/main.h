beaf70d8a9fecb257c70d97afeea249e46e30a83
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-04-29 14:37:19 +0100

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,8 @@
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
-
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
+
 
 #endif
