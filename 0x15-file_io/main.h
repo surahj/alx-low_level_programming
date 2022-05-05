22039e49b894960e7c0c717340c840bd530aa383
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-05-05 05:52:53 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
+char *create_buffer(char *file);
+void close_file(int fd);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
