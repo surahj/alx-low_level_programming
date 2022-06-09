e86ce06192ffa62e70d3ac739061ce0408319d12
surahj <odebodeopeyemi@gmail.com>
2022-06-09 07:15:41 -0700

    Fix error

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the
- * elements of a dlistint_t list
+ * dlistint_len - returns the number of
+ * elements in a linked dlistint_t list
  * @h: pointer to first node of the list
  * Return: number of node in the list
  */
