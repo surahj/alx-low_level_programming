1019e870ae1554692012fdc999da32c7e8d0e12f
Odebode Opeyemi <31376475+surahj@users.noreply.github.com>
2022-06-10 01:12:29 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,31 +12,30 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *h, *new_node;
-	unsigned int i = 0;
+	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	while (temp)
+	for (; idx != 1; idx--)
 	{
-		if (i++ == idx)
-		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			new_node->prev = temp;
-			temp->next->prev = new_node;
-		}
-		temp = temp->next;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
-	if (temp->next == NULL)
+
+	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	return (new_node);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
